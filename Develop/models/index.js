aecf4447d62aab10cd7b3064e929916183afbd2c
BretKruse <krusebret@gmail.com>
2023-09-14 22:51:15 -0500

    final code

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -13,12 +13,14 @@ Category.hasMany(Product, {
   foreignKey: 'category_id',
 });
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(ProductTag, {
-  foreignKey: 'category_id',
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
 });
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(ProductTag, {
-  foreignKey: 'category_id',
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
 });
 
 module.exports = {
